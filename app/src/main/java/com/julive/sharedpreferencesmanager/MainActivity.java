90b9ffd167d705ed46fed16bdf1434e6cf6dcfb5
zhouxuming <zhouxuming@rongcloud.cn>
2019-08-19 18:36:54 +0800

    add sample

--- app/src/main/java/com/julive/sharedpreferencesmanager/MainActivity.java
+++ app/src/main/java/com/julive/sharedpreferencesmanager/MainActivity.java
@@ -1,18 +1,27 @@
 package com.julive.sharedpreferencesmanager;
 
-import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.julive.library.SharedPreferencesManager;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @SuppressLint("CommitPrefEdits")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferencesManager.getInstance().put("keyString", "value");
+        SharedPreferencesManager.getInstance().put("keyInt", 1);
+        SharedPreferencesManager.getInstance().put("keyBoolean", true);
+
+        String s = (String) SharedPreferencesManager.getInstance().get("keyString", "x");
+        int i = (int) SharedPreferencesManager.getInstance().get("keyInt", 0);
+        boolean b = (boolean) SharedPreferencesManager.getInstance().get("keyBoolean", false);
+        Log.e(SharedPreferencesManager.class.getSimpleName(), "String : " + s + " int : " + i + " boolean : " + b);
 
     }
 
